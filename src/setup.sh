2badd451a145ab8e81f5ecb2b40bf89148f57259
Frederic JARDON <frederic.jardon@gmail.com>
2016-09-03 11:56:07 +0000

    [CRON] Simplify arguments to runcron
    Runcron now uses its own install prefix to find the parent
    script directory.

--- src/setup.sh
+++ src/setup.sh
@@ -5,11 +5,13 @@ set -e
 
 # Prepare backups directory
 DATE=$(date '+%Y%m%d')
-BACKUPDIR=~/.backups/${DATE}
+HOUR=$(date '+%H%M%S')
+BACKUPDIR=~/.backups/${DATE}/${HOUR}
 install -m 0700 -d ${BACKUPDIR}
 
 # Backup files
 echo "shell ..."
+cp -f ~/.profile ${BACKUPDIR}
 install -m 0644 dot_profile ~/.profile
 
 # .local setup
@@ -17,6 +19,8 @@ echo "local ..."
 install -m 0755 -d ~/.local/bin
 install -m 0755 -d ~/.local/lib
 install -m 0755 -d ~/.local/share
+install -m 0755 -d ~/.local/var/lock
+install -m 0755 -d ~/.local/var/log
 install -m 0755 -d ~/.local/etc/cron
 install -m 0755 runcron ~/.local/bin
 
@@ -38,16 +42,16 @@ touch ~/.local/etc/cron/environ.bash
 
 crontab -l > "${BACKUPDIR}/crontab"
 tmpcrontab=$(mktemp)
-grep -v ~/.local/etc/cron/ "${BACKUPDIR}/crontab" > "${tmpcrontab}"
-echo "0 * * * *" ~/.local/bin/runcron ~/.local/etc/cron/hourly  >> "${tmpcrontab}"
-echo "0 0 * * *" ~/.local/bin/runcron ~/.local/etc/cron/daily   >> "${tmpcrontab}"
-echo "0 4 * * *" ~/.local/bin/runcron ~/.local/etc/cron/daily-4 >> "${tmpcrontab}"
-echo "0 8 * * *" ~/.local/bin/runcron ~/.local/etc/cron/daily-8 >> "${tmpcrontab}"
-echo "0 12 * * *" ~/.local/bin/runcron ~/.local/etc/cron/daily-12 >> "${tmpcrontab}"
-echo "0 16 * * *" ~/.local/bin/runcron ~/.local/etc/cron/daily-16 >> "${tmpcrontab}"
-echo "0 20 * * *" ~/.local/bin/runcron ~/.local/etc/cron/daily-20 >> "${tmpcrontab}"
-echo "0 0 * * 0" ~/.local/bin/runcron ~/.local/etc/cron/weekly  >> "${tmpcrontab}"
-echo "0 0 1 * *" ~/.local/bin/runcron ~/.local/etc/cron/monthly >> "${tmpcrontab}"
-echo "0 0 1 1 *" ~/.local/bin/runcron ~/.local/etc/cron/yearly  >> "${tmpcrontab}"
+grep -v ~/.local/bin/runcron "${BACKUPDIR}/crontab" > "${tmpcrontab}"
+echo "0  * * * *" ~/.local/bin/runcron hourly   >> "${tmpcrontab}"
+echo "0  0 * * *" ~/.local/bin/runcron daily    >> "${tmpcrontab}"
+echo "0  4 * * *" ~/.local/bin/runcron daily-4  >> "${tmpcrontab}"
+echo "0  8 * * *" ~/.local/bin/runcron daily-8  >> "${tmpcrontab}"
+echo "0 12 * * *" ~/.local/bin/runcron daily-12 >> "${tmpcrontab}"
+echo "0 16 * * *" ~/.local/bin/runcron daily-16 >> "${tmpcrontab}"
+echo "0 20 * * *" ~/.local/bin/runcron daily-20 >> "${tmpcrontab}"
+echo "0  0 * * 0" ~/.local/bin/runcron weekly   >> "${tmpcrontab}"
+echo "0  0 1 * *" ~/.local/bin/runcron monthly  >> "${tmpcrontab}"
+echo "0  0 1 1 *" ~/.local/bin/runcron yearly   >> "${tmpcrontab}"
 crontab "${tmpcrontab}"
 rm -f "${tmpcrontab}"
