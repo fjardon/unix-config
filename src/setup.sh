346806e6197f23336d09664d0cec629fc8abbb94
Frederic JARDON <frederic.jardon@gmail.com>
2016-09-08 05:29:58 +0000

    [SETUP] Move PS0 customization to .bashrc

--- src/setup.sh
+++ src/setup.sh
@@ -3,6 +3,42 @@
 # Exit on any errors
 set -e
 
+# Detect terminal
+# See: http://wiki.bash-hackers.org/scripting/terminalcodes
+TPUT=$(which tput)
+if [ -t 1 -a -n "${TPUT}" ]; then
+    NCOLORS=$(tput colors)
+    if [ -n "${NCOLORS}" -a "${NCOLORS}" -ge 8 ]; then
+        NORMAL_AT=$(tput sgr0)
+        BOLD_AT=$(tput bold)
+        DIM_AT=$(tput dim)
+        STRONG_AT=$(tput smso)
+        ULINE_AT=$(tput smul)
+        BLINK_AT=$(tput blink)
+        REVERSE_AT=$(tput rev)
+        
+        BLACK_FG=$(tput setaf 0)
+        RED_FG=$(tput setaf 1)
+        GREEN_FG=$(tput setaf 2)
+        YELLOW_FG=$(tput setaf 3)
+        BLUE_FG=$(tput setaf 4)
+        MAGENTA_FG=$(tput setaf 5)
+        CYAN_FG=$(tput setaf 6)
+        WHITE_FG=$(tput setaf 7)
+        DEFAULT_FG=$(tput setaf 9)
+        
+        BLACK_BG=$(tput setab 0)
+        RED_BG=$(tput setab 1)
+        GREEN_BG=$(tput setab 2)
+        YELLOW_BG=$(tput setab 3)
+        BLUE_BG=$(tput setab 4)
+        MAGENTA_BG=$(tput setab 5)
+        CYAN_BG=$(tput setab 6)
+        WHITE_BG=$(tput setab 7)
+        DEFAULT_BG=$(tput setab 9)
+    fi
+fi
+
 # Prepare backups directory
 DATE=$(date '+%Y%m%d')
 HOUR=$(date '+%H%M%S')
@@ -11,8 +47,10 @@ install -m 0700 -d ${BACKUPDIR}
 
 # Backup files
 echo "shell ..."
-cp -f ~/.profile ${BACKUPDIR}
-install -m 0644 dot_profile ~/.profile
+cp -f ~/.bash_profile ${BACKUPDIR}
+cp -f ~/.bashrc       ${BACKUPDIR}
+install -m 0644 dot_profile ~/.bash_profile
+install -m 0644 dot_bashrc  ~/.bashrc
 
 # .local setup
 echo "local ..."
@@ -21,6 +59,7 @@ install -m 0755 -d ~/.local/lib
 install -m 0755 -d ~/.local/share
 install -m 0755 -d ~/.local/var/lock
 install -m 0755 -d ~/.local/var/log
+install -m 0755 -d ~/.local/var/run
 install -m 0755 -d ~/.local/etc/cron
 install -m 0755 runcron ~/.local/bin
 
